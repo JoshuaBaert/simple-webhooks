dfedd36e1fdc78cccd07be4cbaf9b5541c4d6b62
Joshyray <developer@baert.io>
2020-02-19 20:36:03 -0700

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const exec = require('child_process').exec;
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+const {port, endpoints} = require('./config');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+function log(txt) {
+    fs.appendFile('log.txt', txt, (err) => {
+        if (err) throw err
+    })
+}
+
+
+endpoints.forEach(({command, url, passPhrase}) => {
+    app.post(url, (req, res, next) => {
+        if(req.body.passPhrase === passPhrase) {
+            let process = exec(command, {}, (err, stdo, stde) => {
+                if(err) {
+                    res.sendStatus(500);
+                    return console.error(err);
+                }
+                if(stdo) log(stdo);
+                if(stde) log(stde);
+                res.sendStatus(200);
+            })
+        } else {
+            res.sendStatus(401)
+        }
+    })
+});
+
+app.listen(port, () => { console.log (`listening on port ${port}`) });
